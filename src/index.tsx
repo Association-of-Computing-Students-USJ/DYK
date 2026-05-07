a4de7a0e8716cb9c604f4168958807aac943de83
FC116338 <sachinikavindya2023@gmail.com>
2026-05-07 22:20:38 +0530

    Delete src directory

--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-// @ts-ignore: side-effect import for CSS module
-import "./index.css";
-import { createRoot } from "react-dom/client";
-import { App } from "./App";
-
-createRoot(document.getElementById("root")!).render(<App />);
